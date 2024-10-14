15b680daac4f9f40bf416c56647d0ad2e1343b1e
priyanka <154439126+Jsnerd-8@users.noreply.github.com>
2024-10-14 13:23:20 +0530

    optimized layout code
     made some changes  to  right-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
 import { Component, HostListener } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import { NgForOf , } from '@angular/common';
 import { ServicesService } from './services.service';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+
+export interface Topic {
+  name: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  topics:  Topic[] = [
+    {name: 'न्यायालय समाचार'},
+    {name: 'क्राइम-कोर्ट न्यूज़'},
+    {name: 'वित्तीय धोखाधड़ी'},
+    {name: 'गैंगस्टर'},
+    {name: 'साइबर हमला'},
+    {name: 'ऑनलाइन धोखाधड़ी'},
+  ];
+
+  
+
   isvisible:boolean=false;
    private currentScroll:number=0
 
@@ -73,7 +92,8 @@ export class AppComponent {
 //  }
 @HostListener('window:scroll', [])
 onWindowScroll(event: Event) {
-  const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  let scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  
   
   console.log('Scroll position:', scrollPosition);
 
@@ -88,12 +108,7 @@ this.isvisible=true
   
  
 }
-// scrollUp(){
-//   if (this.scrollPosition >30) {
-       
-    
-//     this.isvisible=true
-//       }
-
-// }
+ 
+ 
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
  
- 
+import { MatChipsModule } from '@angular/material/chips';
+import { RouterModule } from '@angular/router';
+
  
  
  
 
 @NgModule({
   declarations: [
+   
     AppComponent,
     HomeComponent,
+
     
   
   ],
-  imports: [
+  imports: [ RouterModule,
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatChipsModule,
    
     
     
