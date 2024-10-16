85b459fdada1c122ceea0d7208509da02b480430
Ashish <ashishlowanshi1998@gmail.com>
2024-10-16 16:03:31 +0530

    changes in left navbar, top nav

--- src/app/shared/components/left-navbar/left-navbar.component.ts
+++ src/app/shared/components/left-navbar/left-navbar.component.ts
@@ -6,79 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-navbar.component.css']
 })
 export class LeftNavbarComponent implements OnInit {
-  headers=[
-    {
-      icon:'fa-solid fa-house menu-icon pe-2',
-      title:'होम',
-       
-
-    },
-    {
-      icon:'fas fa-play-circle pe-2',
-      title:'वीडियो',
-      state:'वीडियो',
-       
-
-    },
-    {
-      icon:'fas fa-newspaper pe-2',
-      title:'विज़ुअल स्टोरीज़',
-       
-
-    },
-    {
-      icon:'fa-solid fa-house menu-icon pe-2',
-      title:'होम',
-       
-
-    },
-    {
-      icon:'fas fa-play-circle pe-2',
-      title:'राज्य ',
-       
-
-    },
-    {
-      icon:'fas fa-newspaper pe-2',
-      title:'विज़ुअल स्टोरीज़',
-       
-
-    },
-    {
-      icon:'fa-solid fa-house menu-icon pe-2',
-      title:'होम',
-       
-
-    },
-    {
-      icon:'fas fa-play-circle pe-2',
-      title:'वीडियो',
-       
-
-    },
-    {
-      icon:'fas fa-newspaper pe-2',
-      title:'विज़ुअल स्टोरीज़',
-       
-
-    },
-    {
-      icon:'fas fa-newspaper pe-2',
-      title:'विज़ुअल स्टोरीज़',
-       
-
-    },
-    {
-      icon:'fas fa-newspaper pe-2',
-      title:'विज़ुअल स्टोरीज़',
-       
-
-    },
-   ]
+  selectedIndex:any= -1;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
+  
 
+  headers = [
+    { icon: 'fa-solid fa-house menu-icon pe-2', title: 'होम' },
+    { icon: 'fas fa-play-circle pe-2', title: 'वीडियो' },
+    { icon: 'fas fa-newspaper pe-2', title: 'विज़ुअल स्टोरीज़' },
+    { icon: 'fa-solid fa-house menu-icon pe-2', title: 'होम' },
+    { icon: 'fas fa-play-circle pe-2', title: 'राज्य', id: '5' },
+    { icon: 'fas fa-newspaper pe-2', title: 'विज़ुअल स्टोरीज़' },
+    { icon: 'fas fa-newspaper pe-2', title: 'विज़ुअल स्टोरीज़' },
+    { icon: 'fas fa-play-circle pe-2', title: 'वीडियो' },
+    { icon: 'fas fa-play-circle pe-2', title: 'वीडियो' },
+  
+    
+
+  ];
+
+  states = [
+    'आंध्र प्रदेश', 'अरुणाचल प्रदेश', 'असम', 'बिहार', 'छत्तीसगढ़', 'गोवा', 'गुजरात', 
+    'हरियाणा', 'हिमाचल प्रदेश', 'झारखंड', 'कर्नाटक', 'केरल', 'मध्य प्रदेश', 
+    'महाराष्ट्र', 'मणिपुर', 'मेघालय', 'मिज़ोरम', 'नागालैंड', 'ओड़ीशा', 'पंजाब', 
+    'राजस्थान', 'सिक्किम', 'तमिलनाडु', 'तेलंगाना', 'त्रिपुरा', 'उत्तराखंड', 
+    'उत्तर प्रदेश', 'पश्चिम बंगाल'
+  ];
+  
+
+
+
+  selectItem(index: number) {
+    this.selectedIndex = index;
+  }
 }
+
